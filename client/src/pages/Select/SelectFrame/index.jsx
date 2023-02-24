73d9e2a1f18fcd0256b6827c6d1b2d425444aea3
ekdms5566 <ekdms5566@naver.com>
2023-02-24 22:36:20 +0900

    Feat : 갤러리 이미지 업로드 기능 구현

--- client/src/pages/Select/SelectFrame/index.jsx
+++ client/src/pages/Select/SelectFrame/index.jsx
@@ -1,14 +1,24 @@
-import { Link } from "react-router-dom";
+import { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Frame from "../../../assets/img-frame2.svg";
 import Button from "../../../components/Button";
 import * as S from "../style";
 
 export default function SelectFrame() {
+  const navigate = useNavigate();
+  const imgRef = useRef();
+
+  const handlePickImg = (e) => {
+    const file = URL.createObjectURL(imgRef.current.files[0])
+    file && navigate('/frame', { state : file })
+  }
+
   return (
     <S.Wrapper>
       <S.Subtit>나만의 벚꽃필름 프레임을 선택해보세요!</S.Subtit>
       <S.SubLogoImg src={Frame} alt="" />
-      <Button text="갤러리에서 직접 선택" />
+      <S.UploadInp type="file"name="imageUpload" accept="image/*" ref={imgRef} onChange={handlePickImg} />
+      <Button text="갤러리에서 직접 선택" onClick={()=> imgRef.current.click()}/>
       <Link to="/selectFrametype" >
         <Button text="벚꽃필름 전용 프레임 선택"/>
       </Link>
